d3e0a4a491706c9f8eeb534b004c2ff3859cfc9b
Arian Valles <arian.valles@outlook.com>
2017-04-13 02:04:16 -0300

    Subiendo lo que hicimos en clases al proyecto.

--- Ejercicio 1/src/Ejercicio1.java
+++ Ejercicio 1/src/Ejercicio1.java
@@ -1,10 +1,61 @@
+import java.util.*;
+import java.util.Random;
 
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hola mundo");
+		//Declaracion de variables
+		int poblacion[][] = new int[10][30] ;
+		
 
+		//Programa
+		for (int i = 0; i < 10; i++){
+			for (int j = 0;j < 30; j++){
+				Random rnd = new Random();
+				int x = rnd.nextInt(2);
+				poblacion[i][j] = x;
+			}
+			System.out.println(Arrays.toString(poblacion[i]));	
+		}
+		
+		System.out.println("--------------------------------------------------------------");
+		
+		//int a=Integer.parseInt(Arrays.toString(poblacion[0]));
+		//System.out.println(a);
+		
+		for(int h = 0; h < 10; h++){
+			int entero = aEntero(poblacion[h]);
+			System.out.println(entero);
+		}
+		
 	}
-
+	
+	//Declaracion de metodos
+	
+	int objetivo (){
+		int y;
+		
+		Random rnd = new Random();
+		int coef = (int) ((Math.pow(2, 30))-1);
+		int x = rnd.nextInt();
+		
+		y = (int)Math.pow( (x/coef) ,2);
+				
+		return y;
+	}
+	
+	static int aEntero (int algo[])
+	{
+		int k=0, exp=0;
+		
+		for(int i = 29; i <= 0; i--){
+			if(algo[i] == 1){
+				k=k+(int) Math.pow(2, exp);
+				exp++;
+			}
+		}
+		return k;
+	}
 }
+
