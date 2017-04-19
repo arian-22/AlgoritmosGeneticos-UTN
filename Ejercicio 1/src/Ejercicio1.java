54aa79b294e536dc2991e286f154c23f8dcb15ee
Arian Valles <arian.valles@outlook.com>
2017-04-18 22:06:08 -0300

    Limpieza de codigo.

--- Ejercicio 1/src/Ejercicio1.java
+++ Ejercicio 1/src/Ejercicio1.java
@@ -1,63 +1,67 @@
-import java.util.*;
 import java.util.Random;
 
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
 		
-		//Declaracion de variables
-		int poblacion[][] = new int[10][30] ;
-		
+		//Declaracion de variables.
+		int poblacionInicial[][] = new int[10][30];
 
-		//Programa
+		/*
+		 * Programa principal
+		 * */
+		
+		//Creacion de la poblacion original.
+		System.out.println("POBLACIÓN INICIAL:");
+		System.out.println("------------------");
 		for (int i = 0; i < 10; i++){
 			for (int j = 0;j < 30; j++){
 				Random rnd = new Random();
 				int x = rnd.nextInt(2);
-				poblacion[i][j] = x;
+				poblacionInicial[i][j] = x;
 			}
-			System.out.println(Arrays.toString(poblacion[i]));	
+			//System.out.println(Arrays.toString(poblacion[i]));
+			System.out.println("Nro. "+(i+1)+": " + devuelveBinario(poblacionInicial[i]));
 		}
-		
-		System.out.println("--------------------------------------------------------------");
-		
-		//int a=Integer.parseInt(Arrays.toString(poblacion[0]));
-		//System.out.println(a);
+		System.out.println("_____________________________________________________\n");
 		
 		for(int h = 0; h < 10; h++){
-			long entero = aEntero(poblacion[h]);
-			System.out.println(entero);
+			long entero = aEntero(poblacionInicial[h]);
+			System.out.println("#"+(h+1));
+			System.out.println("Cromosoma BINARIO: " + devuelveBinario(poblacionInicial[h]));
+			System.out.println("Cromosoma en DECIMAL: " + entero);
+			System.out.println("");
 		}
-		
 	}
 	
 	//Declaracion de metodos
 	
-	int objetivo (){
+	int funcionObjetivo (){
 		int y;
-		
+
 		Random rnd = new Random();
 		int coef = (int) ((Math.pow(2, 30))-1);
 		int x = rnd.nextInt();
 		
-		y = (int)Math.pow( (x/coef) ,2);
-				
+		y = (int)Math.pow( (x/coef) , 2);
+		
 		return y;
 	}
 	
-	static long aEntero (int[] algo)
-	{
+	static long aEntero (int[] array){
 		
-		String cromosoma = "";
-		for(int i = 0 ; i < 30; i++){
-			cromosoma = cromosoma + algo[i];
-		}
-		long decimalConv =Integer.parseInt(cromosoma, 2);
-		System.out.println("DECIMAL CONVERTIDO: "+decimalConv);
-		System.out.println("Cromosoma: " + cromosoma);
-		
+		String cromosoma = devuelveBinario(array);	
+		long decimalConv = Integer.parseInt(cromosoma, 2);
 		
 		return decimalConv;
 	}
+	
+	private static String devuelveBinario(int[] array){
+		
+		String cromosoma = "";
+		for(int i = 0 ; i < 30; i++){
+			cromosoma = cromosoma + array[i];
+		}
+		return cromosoma;
+	}
 }
-
